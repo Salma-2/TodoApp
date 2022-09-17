8576a2d2d800af83490c1d1133cd52076c43d720
= <=>
2022-09-17 12:47:09 +0200

    added tests for the viewModel

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,18 +1,47 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.CoreMatchers.*
+import org.junit.Assert.assertThat
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
+
+    // Executes each task synchronously using Architecture Components.
+    @get: Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    // GIVEN a fresh ViewModel (subject under the test)
+    private lateinit var tasksViewModel: TasksViewModel
+
+    @Before
+    fun setupViewModel() {
+        tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+    }
+
     @Test
     fun addNewTask_setNewTaskEvent() {
-        // GIVEN a fresh ViewModel
-        val taskViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
         // WHEN adding a new task
-
+        tasksViewModel.addNewTask()
         // THEN the new task event is triggered
+        val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
+        assertThat(value.getContentIfNotHandled(), (not(nullValue())))
+    }
+
+    // check that if you've set your filter type to show all tasks, that the Add task button is visible.
+    @Test
+    fun setFilterAllTasks_tasksAddViewVisible() {
+        // When the filter type is ALL_TASKS
+        tasksViewModel.setFiltering(TasksFilterType.ALL_TASKS)
+
+        // Then the "Add task" action is visible
+        val value = tasksViewModel.tasksAddViewVisible.getOrAwaitValue()
+        assertThat(value, `is`(true))
     }
 }
